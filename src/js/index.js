b50c0bc0d866f06a2d281f8e4b937decad4ca826
Mateusz Urban <98m.urban@gmail.com>
2020-01-16 22:26:48 +0100

    styled mobile menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,17 @@
 import "../style/main.scss";
 
-const toggleNavBar = () => {
-  const links = document.querySelector(".navbar_list");
-  if (links.style.display === "block") {
-    links.style.display = "none";
-  } else {
-    links.style.display = "block";
-  }
+const showNav = () => {
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".navbar_list_items");
+  // toggle hamburger navbar
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    // hamburger animation
+    hamburger.classList.toggle("toggle");
+  });
+
+
 };
- toggleNavBar();
+
+showNav();
