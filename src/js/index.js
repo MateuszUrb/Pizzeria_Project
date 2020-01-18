517af895c57add9517ead9852a3fd528b807beaa
Mateusz Urban <98m.urban@gmail.com>
2020-01-18 22:39:44 +0100

    styling navbar and welcome section for better design,

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,19 @@ import "../style/main.scss";
 const showNav = () => {
   const hamburger = document.querySelector(".hamburger");
   const nav = document.querySelector(".navbar_list_items");
+  const listItem = document.querySelectorAll(".list_item");
   // toggle hamburger navbar
   hamburger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
 
+    for (let i = 0; i < listItem.length; i++) {
+      listItem[i].style.width = "100%";
+    }
     // hamburger animation
     hamburger.classList.toggle("toggle");
   });
-
-
 };
 
 showNav();
+
+// navbar background change after scroll
--- webpack.common.js
+++ webpack.common.js
@@ -26,7 +26,8 @@ module.exports = {
                     loader: "file-loader",
                     options: {
                         name: "[name].[hash].[ext]",
-                        outputPath: "imgs"
+                        outputPath: "imgs",
+                        esModule: false
                     }
                 }
             }
