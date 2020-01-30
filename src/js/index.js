46f3f2f64df707aee72f776cf6f62e6c1f252ec7
Mateusz Urban <98m.urban@gmail.com>
2020-01-30 23:15:40 +0100

    messing with hero image under navBar

--- src/js/index.js
+++ src/js/index.js
@@ -4,18 +4,22 @@ const showNav = () => {
   const hamburger = document.querySelector(".hamburger");
   const nav = document.querySelector(".navbar_list_items");
   const listItem = document.querySelectorAll(".list_item");
+  const navHeader = document.querySelector("header");
   // toggle hamburger navbar
+
   hamburger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
-
     for (let i = 0; i < listItem.length; i++) {
       listItem[i].style.width = "100%";
+      listItem[0].style.borderTop = "1px solid black";
     }
+
     // hamburger animation
     hamburger.classList.toggle("toggle");
+    navHeader.classList.toggle("navHeaderOpacity");
   });
 };
 
 showNav();
-
 // navbar background change after scroll
+
